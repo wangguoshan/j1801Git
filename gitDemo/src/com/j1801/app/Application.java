a46060d098923cae0f5cc8ee20ac29fcacaf27e5
WINDOWS-8O825V7\Administrator <526778065@qq.com>
2018-06-21 17:18:42 +0800

    for大傻逼循环

--- gitDemo/src/com/j1801/app/Application.java
+++ gitDemo/src/com/j1801/app/Application.java
@@ -3,8 +3,15 @@ package com.j1801.app;
 public class Application {
 
 	public static void main(String[] args) {
+		Demo01();
 
+	}
 
+	private static void Demo01() {
+		for (int i = 0; i < 20; i++) {
+			System.out.println("你是一个大傻逼");
+		}
+		
 	}
 
 }
